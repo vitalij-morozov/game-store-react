c8f32b8c37e617102cd9044eff5aacd3fa2b5522
Vitalij <vitaliy.morozov1@gmail.com>
2022-12-23 14:28:42 +0200

    setting up backend side

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_DB_KEY)
+  .then(() => {
+    console.log('mongoose connection is established');
+  })
+  .catch((error) => {
+    console.warn('mongoose connection error: ' + error);
+  });
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log('server is running on port ' + port);
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found 404' });
+});
